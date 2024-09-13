676147f84b8a383d4cbb5317a161aed5b40e864b
Cole Arendt <cole.arendt@outlook.com>
2024-09-13 02:25:32 -0400

    tweak the app a bit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import chatstream
+import dotenv
 import os
 
 from shiny import App, Inputs, Outputs, Session, ui
 
-import chatstream
-
+dotenv.load_dotenv()
 base_url = os.getenv("OPENAI_URL")
+print(f'Using url: {base_url}')
 
 if base_url == "":
     raise ValueError("OPENAI_URL environment variable must be set")
@@ -13,7 +15,9 @@ app_ui = ui.page_fixed(
     chatstream.chat_ui("mychat"),
 )
 
+
 def server(input: Inputs, output: Outputs, session: Session):
     chatstream.chat_server("mychat", url=base_url)
+
 
 app = App(app_ui, server)
